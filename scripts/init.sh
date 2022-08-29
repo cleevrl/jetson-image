e9ea4ba25ff640fa5996517b5a0398b76ee71087
cleverlee <cleverlee153@gmail.com>
2022-08-29 19:32:52 +0900

    fix: Fix script, make logs dir

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,14 +3,14 @@
 echo "dongbuict0" | sudo -S apt-get update
 echo "dongbuict0" | sudo -S apt-get install -y netplan.io python3-pip python3-dev filezilla
 
-mkdir ~/lds-nano/lds-logs
+mkdir ~/lds-logs
 
 pip3 install --upgrade pip
 python3 -m pip install docker-compose
 
 echo "dongbuict0" | sudo -S -H pip3 install -U jetson-stats
 
-wget "https://download.nomachine.com/download/7.10/Arm/nomachine_7.10.1_1_arm64.deb" ~/Downloads/nomachine.deb
+wget "https://download.nomachine.com/download/7.10/Arm/nomachine_7.10.1_1_arm64.deb" -P ~/Downloads/nomachine.deb
 echo "donbguict0" | sudo dpkg -i ~/Downloads/nomachine.deb
 
 cp ~/.bashrc ~/.bashrc_backup
@@ -19,7 +19,7 @@ echo 'echo "dongbuict0" | sudo -S chmod 666 /var/run/docker.sock' >> ~/.bashrc
 echo 'cd ~/left-detection/startup' >> ~/.bashrc
 echo 'source ./lds_start.sh' >> ~/.bashrc
 
-cp ./net_config/net_nano_10.yaml /etc/netplan/config.yaml
+edho "dongbuict0" | sudo -S cp ./net_config/net_nano_10.yaml /etc/netplan/config.yaml
 echo "dongbuict0" | sudo -S netplan apply
 
 git clone https://github.com/Pyrestone/jetson-fan-ctl ~/jetson-fan-ctl
